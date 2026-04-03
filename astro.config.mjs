c950b25237f89ebcaff66bd38755b707e92a25f3
social-archive <dj500dj100@gmail.com>
2026-04-03 10:26:59 +0900

    feat: add base layout with SEO metadata and Google Analytics integration using Partytown and Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
